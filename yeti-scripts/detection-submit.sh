74a9ae85a44905c5c0dd505ff0625799f05e17d3
Shababo <benshababo@gmail.com>
2015-07-14 18:12:09 -0400

    ignore data, submit script started

---
+++ yeti-scripts/detection-submit.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+#detection-submit.sh
+#Torque script to run Matlab program
+
+#Torque directives
+#PBS -N psc-detection
+#PBS -W group_list=yetistats
+#PBS -l nodes=1:ppn=12,walltime=04:00:00,mem=16000mb
+#PBS -M shababo@berkeley.edu
+#PBS -m abe
+#PBS -V
+
+#set output and error directories (SSCC example here)
+#PBS -o localhost:/vega/stats/users/bms2156/log
+#PBS -e localhost:/vega/stats/users/bms2156/log
+
+#Command to execute Matlab code
+matlab -nosplash -nodisplay -nodesktop -r "detect_pscs_cluster('$trace_file','$ref_file','$corr_file')" > matoutfile.$PBS_ARRAYID
+
+#Command below is to execute Matlab code for Job Array (Example 4) so that each part writes own output
+#matlab -nosplash -nodisplay -nodesktop -r "simPoissGLM($LAMBDA)" > matoutfile.$PBS_ARRAYID
+
+#End of script
