d9047721764dc5d5da94a5d1af1eae9e49a052cd
Shababo <benshababo@gmail.com>
2015-07-14 20:03:37 -0400

    update to submission script

--- yeti-scripts/detection-submit.sh
+++ yeti-scripts/detection-submit.sh
@@ -15,7 +15,7 @@
 #PBS -e localhost:/vega/stats/users/bms2156/log
 
 #Command to execute Matlab code
-matlab -nosplash -nodisplay -nodesktop -r "detect_pscs_cluster('$trace_file','$param_file', $param_ind)" > matoutfile.$PBS_ARRAYID
+matlab -nosplash -nodisplay -nodesktop -r "detect_pscs('$trace_file','$param_file', $param_ind)" > matoutfile.$PBS_ARRAYID
 
 #Command below is to execute Matlab code for Job Array (Example 4) so that each part writes own output
 #matlab -nosplash -nodisplay -nodesktop -r "simPoissGLM($LAMBDA)" > matoutfile.$PBS_ARRAYID
