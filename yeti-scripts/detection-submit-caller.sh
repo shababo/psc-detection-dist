73a32ae06227506a07d942e7647a1ae73eeef131
Shababo <benshababo@gmail.com>
2015-07-16 01:13:58 -0400

    more submission bug fixes

--- yeti-scripts/detection-submit-caller.sh
+++ yeti-scripts/detection-submit-caller.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 
-trace_file = "/vega/stats/users/bms2156/psc-detection/data/psc_traceset_01.mat"
-param_file = "/vega/stats/users/bms2156/psc-detection/data/psc_parameters_test01.mat"
-noise_type = 3
+trace_file="/vega/stats/users/bms2156/psc-detection/data/psc_traceset_01.mat"
+param_file="/vega/stats/users/bms2156/psc-detection/data/psc_parameters_test01.mat"
+noise_type=3
 
 for i in `seq 1 4`; #486
 do
-	echo $i
-	echo $trace_file
-	echo $param_file
-	echo $noise_type
-	
-	response=`qsub -v trace_file=trace_file,param_file=param_file,param_ind=i,noise_type=noise_type /vega/stats/users/bms2156/psc-detection/yeti-scripts/detection-submit.sh`
+
+	response=`qsub -v trace_file=$trace_file,param_file=$param_file,param_ind=$i,noise_type=$noise_type /vega/stats/users/bms2156/psc-detection/yeti-scripts/detection-submit.sh`
 	echo $response
 	sleep 5 
 done
--- yeti-scripts/detection-submit.sh
+++ yeti-scripts/detection-submit.sh
@@ -3,7 +3,7 @@
 #Torque script to run Matlab program
 
 #Torque directives
-#PBS -N psc-detection-$param_ind
+#PBS -N psc-detection
 #PBS -W group_list=yetistats
 #PBS -l nodes=1:ppn=12,walltime=04:00:00,mem=16000mb
 #PBS -M shababo@berkeley.edu
@@ -15,7 +15,11 @@
 #PBS -e localhost:/vega/stats/users/bms2156/log
 
 #Command to execute Matlab code
-matlab -nosplash -nodisplay -nodesktop -r "detect_pscs('$trace_file','$param_file', $param_ind, $noise_type)" > matoutfile.$PBS_ARRAYID
+echo $trace_file
+echo $param_file
+echo $param_ind
+echo $noise_type
+matlab -nosplash -nodisplay -nodesktop -r "detect_pscs('$trace_file','$param_file', $param_ind, $noise_type)" > matoutfile
 
 #Command below is to execute Matlab code for Job Array (Example 4) so that each part writes own output
 #matlab -nosplash -nodisplay -nodesktop -r "simPoissGLM($LAMBDA)" > matoutfile.$PBS_ARRAYID
