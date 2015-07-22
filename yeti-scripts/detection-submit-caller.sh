e662ccc1ae4d505ae5678f561848dbda8b55697e
Shababo <benshababo@gmail.com>
2015-07-22 18:49:39 -0400

    tidying up cluster scripts

--- yeti-scripts/detection-submit-caller.sh
+++ yeti-scripts/detection-submit-caller.sh
@@ -4,7 +4,7 @@ trace_file="/vega/stats/users/bms2156/psc-detection/data/psc_traceset_test.mat"
 param_file="/vega/stats/users/bms2156/psc-detection/data/psc_parameters_test01.mat"
 noise_type=3
 
-for i in `seq 1 10`; #486
+for i in `seq 1 486`; #486
 do
 
 	response=`qsub -v trace_file=$trace_file,param_file=$param_file,param_ind=$i,noise_type=$noise_type /vega/stats/users/bms2156/psc-detection/yeti-scripts/detection-submit.sh`
