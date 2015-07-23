2ba64eeb19267308bb9aa2f19d3515403c007dd7
Josh <jsmerel@gmail.com>
2015-07-23 18:52:47 -0400

    there was a bug when uploaded before

--- PlotMCMC.m
+++ PlotMCMC.m
@@ -3,12 +3,7 @@
 figure(1)
 burnIn = round(2/5*length(results.trials.tau));
 nBins = length(trace);
-%ML - DOF, init, baseline and each burst amplitude
-% SSE=sum((trace-trials.curves{end}).^2);
-% n=numel(trials.curves{end})-(2+mcmc.N_sto(end)); 
-% final_var = SSE/n;%standard error
-%
-% final_std = sqrt(final_var);
+
 plot(trace,'ko'); 
 modelledTrace = [];
 hold on
@@ -22,10 +17,6 @@ end
 MTm = mean(modelledTrace);
 MTs = std(modelledTrace);%estimation noise
 plot(MTm,'g');
-%Liams idea: posterior uncertaubnty which is the sum of uncertainty in
-%Ca signal + inherent noise of calcium signal
-plot(MTm+2*(final_std+MTs),'r--'); 
-plot(MTm-2*(final_std+MTs),'r--'); 
 hold off
 axis tight
 hold off
