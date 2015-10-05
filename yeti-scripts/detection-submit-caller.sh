19a59e46f4d11d1a75ecded6c6e104aab172c3c3
shababo <benshababo@gmail.com>
2015-10-04 21:05:35 -0700

    wrote first pass at new cluster code

--- yeti-scripts/detection-submit-caller.sh
+++ yeti-scripts/detection-submit-caller.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-trace_file="/vega/stats/users/bms2156/psc-detection/data/psc_traceset_test.mat"
-param_file="/vega/stats/users/bms2156/psc-detection/data/psc_parameters_test01.mat"
-noise_type=1
+#trace_file="/vega/stats/users/bms2156/psc-detection/data/psc_traceset_test.mat"
+#param_file="/vega/stats/users/bms2156/psc-detection/data/psc_parameters_test01.mat"
+#noise_type=1
 
-for i in `seq 1 20`; #486
+ for f in "$dir"/*; do echo "$f"; done
+
+dir = "/vega/stats/users/bms2156/psc-detection/data/cluster-param-files"
+for param_file in "$dir"/*; #486
 do
 
-	response=`qsub -v trace_file=$trace_file,param_file=$param_file,param_ind=$i,noise_type=$noise_type /vega/stats/users/bms2156/psc-detection/yeti-scripts/detection-submit.sh`
+	response=`qsub -v param_file=$param_file /vega/stats/users/bms2156/psc-detection/yeti-scripts/detection-submit.sh`
 	echo $response
 	sleep 5 
 done
-
