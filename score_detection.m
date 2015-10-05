a63c1134968c44b53edf6cc33956ea493a512c5e
shababo <benshababo@gmail.com>
2015-10-05 15:42:38 -0700

    get roc from experiment

--- score_detection.m
+++ score_detection.m
@@ -2,8 +2,8 @@ function score_detection(resultsfile, tolerance, plot_results)
 
 load(resultsfile)
 params.timing_score_tolerance = tolerance;
-load(params.traces_filename,'true_event_times')
-% load('/home/shababo/projects/mapping/code/psc-detection/data/simulated-data-longer-traces-epsc.mat')
+% load(params.traces_filename,'true_event_times')
+load('/home/shababo/projects/mapping/code/psc-detection/data/simulated-data-longer-traces-epsc.mat')
 
 num_traces = length(results);
 
@@ -21,12 +21,6 @@ save(resultsfile,'timing_score','-append')
 if plot_results
     
     figure
-    for i = 1:num_traces
-        est_times = results(i).trials.times{map_ind};
-        scatter(true_event_times{i},-i*ones(size(true_event_times{i})),'b*')
-        hold on
-        scatter(est_times,-i*ones(size(est_times)),'ro')
-        hold on
-    end
-    hold off
+    plot_score_detection(results,true_event_times,timing_score)
+    
 end
