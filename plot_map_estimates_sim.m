eec100cf77b3e5a443522e90b389fd99d3c67270
shababo <benshababo@gmail.com>
2015-10-05 20:13:32 -0700

    to merge

--- plot_map_estimates_sim.m
+++ plot_map_estimates_sim.m
@@ -1,7 +1,11 @@
 function plot_map_estimates_sim(results_file, trace_offset, varargin)
 
 load(results_file)
-load(params.traces_filename)
+try
+    load(params.traces_filename)
+catch
+    load('data/sim-temp.mat')
+end
 
 if ~isfield(params,'start_ind')
     params.start_ind = 1;
@@ -70,11 +74,11 @@ axes(ax1) % sets ax1 to current axes
 text(.025,0.6,descr)
 
 axes(ax2)
-plot_trace_stack(traces,zeros(size(traces)),[],bsxfun(@plus,zeros(length(traces),3),[1 .4 .4]),[],size(traces,2)-1,trace_offset,'-')
+plot_trace_stack(traces,zeros(size(traces)),[],bsxfun(@plus,zeros(length(traces),3),[1 .4 .4]),[],size(traces,2)-1,trace_offset);%,'-')
 hold on
-plot_trace_stack(params.event_sign*map_curves,zeros(size(traces)),[],bsxfun(@plus,zeros(length(traces),3),[0 0 1]),[],size(traces,2)-1,trace_offset,'-')
+plot_trace_stack(params.event_sign*map_curves,zeros(size(traces)),[],bsxfun(@plus,zeros(length(traces),3),[0 0 1]),[],size(traces,2)-1,trace_offset);%,'-')
 hold on
-plot_trace_stack(true_signal,zeros(size(traces)),[],bsxfun(@plus,zeros(length(traces),3),[0 1 0]),[],size(traces,2)-1,trace_offset,'--')
+plot_trace_stack(true_signal,zeros(size(traces)),[],bsxfun(@plus,zeros(length(traces),3),[0 1 0]),[],size(traces,2)-1,trace_offset);%,'--')
 hold off
 
 title(strrep(results_file,'_','-'))
