37c06d8ab459c81a5afe5f44a10ad26baad29419
shababo <benshababo@gmail.com>
2015-10-19 17:00:54 -0700

    added files for scoring alt methods

---
+++ compute_roc_deconv.m
@@ -0,0 +1,23 @@
+function roc = compute_roc_deconv(filename)
+
+load(filename)
+
+roc = zeros(length(threshold),length(min_spacing),2);
+
+for thresh_i = 1:length(threshold)
+    for min_spacing_i = 1:length(min_spacing)
+       
+        if ~exist('timing_score','var')
+            disp(['not scored: '])
+            continue
+        end
+        
+        for j = 1:length(event_times{thresh_i,min_spacing_i})
+            
+            num_est_events = length(event_times{thresh_i,min_spacing_i}{j});
+            roc(thresh_i,min_spacing_i,1) = roc(thresh_i,min_spacing_i,1) + num_est_events - length(timing_score(thresh_i,min_spacing_i,j).correct_inds);
+            roc(thresh_i,min_spacing_i,2) = roc(thresh_i,min_spacing_i,2) + length(timing_score(thresh_i,min_spacing_i,j).correct_inds);
+            
+        end
+    end
+    endd
