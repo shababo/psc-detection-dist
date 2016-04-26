17ed252265bd1000288884efbd828f68ef53cc54
shababo <benshababo@gmail.com>
2016-04-25 17:01:40 -0700

    uhh...

--- build_spatial_posteriors.m
+++ build_spatial_posteriors.m
@@ -5,10 +5,10 @@ spatial_posteriors = struct();
 
 figure(9991)
 subplot1(size(posteriors_grid,1), size(posteriors_grid,2), 'Gap', [.005 .005], 'XTickL', 'Margin', 'YTickL', 'Margin');
-figure(9992)
-subplot1(size(posteriors_grid,1), size(posteriors_grid,2), 'Gap', [.005 .005], 'XTickL', 'Margin', 'YTickL', 'Margin');
-figure(9993)
-subplot1(size(posteriors_grid,1), size(posteriors_grid,2), 'Gap', [.005 .005], 'XTickL', 'Margin', 'YTickL', 'Margin');
+% figure(9992)
+% subplot1(size(posteriors_grid,1), size(posteriors_grid,2), 'Gap', [.005 .005], 'XTickL', 'Margin', 'YTickL', 'Margin');
+% figure(9993)
+% subplot1(size(posteriors_grid,1), size(posteriors_grid,2), 'Gap', [.005 .005], 'XTickL', 'Margin', 'YTickL', 'Margin');
 figure(99944)
 subplot1(size(posteriors_grid,1), size(posteriors_grid,2), 'Gap', [.005 .005], 'XTickL', 'Margin', 'YTickL', 'Margin');
 figure(9995)
@@ -46,42 +46,42 @@ for i = 1:size(posteriors_grid,1)
                         
             figure(9991)
             subplot1((i-1)*size(posteriors_grid,2) + j);
-            histogram(spatial_posteriors(i,j).amps,[20:20:300])
+            histogram(spatial_posteriors(i,j).amps,[20:3:300],'Normalization','countdensity')
             axis off
-            ylim([0 10000])
-%             xlim([20 250])
+            ylim([0 40]*10*3)
+            xlim([20 300])
             
-            figure(9992)
-            subplot1((i-1)*size(posteriors_grid,2) + j);
-            histogram(spatial_posteriors(i,j).num_events,0:8)
-            axis off
-
-            ylim([0 10000])
-%             xlim([0 7])
-            
-            figure(9993)
-            subplot1((i-1)*size(posteriors_grid,2) + j);
-            histogram(spatial_posteriors(i,j).tau1,[5:5:50])
-            axis tight
-            axis off
-            ylim([0 10000])
-%             xlim([5 50])
-            
+%             figure(9992)
+%             subplot1((i-1)*size(posteriors_grid,2) + j);
+%             histogram(spatial_posteriors(i,j).num_events,0:8,'Normalization','countdensity')
+%             axis off
+% 
+%             ylim([0 40]*10)
+%             xlim([0 8])
+% %             
+%             figure(9993)
+%             subplot1((i-1)*size(posteriors_grid,2) + j);
+%             histogram(spatial_posteriors(i,j).tau1,[5:5:50],'Normalization','countdensity')
+%             axis tight
+%             axis off
+%             ylim([0 10000])
+% %             xlim([5 50])
+%             
             figure(99944)
             subplot1((i-1)*size(posteriors_grid,2) + j);
-            histogram(spatial_posteriors(i,j).tau2,[30:20:300])
+            histogram(spatial_posteriors(i,j).tau2,[30:5:600],'Normalization','countdensity')
             axis tight
             axis off
-            ylim([0 10000])
-%             xlim([30 200])
+            ylim([0 40]*10*3)
+            xlim([30 600])
             
 
             figure(9995)
             subplot1((i-1)*size(posteriors_grid,2) + j);
-            histogram(spatial_posteriors(i,j).times,[0:.005:.1]*20000)
+            histogram(spatial_posteriors(i,j).times,(0:.001:.1)*20000,'Normalization','countdensity') %
             axis off
-            ylim([0 10000])
-%             xlim([0 .1]*20000)
+            ylim([0 25]*10*3)
+            xlim([0 .1]*20000)
             
         end
     end
@@ -90,20 +90,20 @@ end
 figure(9991)
 subplot1(1);
 title('amps')
-
-figure(9992)
-subplot1(1)
-title('num events')
-
-figure(9993)
-subplot1(1)
-histogram(spatial_posteriors(i,j).tau1)
-title('tau on')
-
+% 
+% figure(9992)
+% subplot1(1)
+% title('num events')
+% 
+% figure(9993)
+% subplot1(1)
+% histogram(spatial_posteriors(i,j).tau1)
+% title('tau on')
+% 
 figure(99944)
 subplot1(1)
 title('tau off')
-
+% 
 figure(9995)
 subplot1(1)
 title('times')
